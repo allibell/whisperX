6fecb5f950b615b63e4b396e63c3e57e53cdcfb0
Allison Bellows <alibellows@gmail.com>
2024-01-17 00:32:44 -0800

    named diarization hack
    add first full transcription demo
    
    add improved demo

--- make_movie.py
+++ make_movie.py
@@ -2,6 +2,7 @@ import argparse
 from moviepy.editor import AudioFileClip, concatenate_videoclips, TextClip, CompositeVideoClip, ImageClip
 import numpy as np
 import srt
+import re
 
 def filter_subtitles(subtitles, start_time, end_time):
     filtered_subtitles = []
@@ -27,7 +28,33 @@ def create_video_with_captions(audio_file_path, subtitles_file_path, output_file
 
     # Create video clips with subtitles
     clips = []
+    doctor_name = None
+    doctor_speaker_id = None
+    patient_speaker_id = None
     for subtitle in subtitles:
+        # Hilarious hack
+        # Check if the Doctor is speaking by searching for "Doctor" in the subtitle
+        # If so, we map their diarization speaker ID (e.g. [SPEAKER_01]) to the Doctor's name, and
+        # the other speaker's ID to "Patient"
+        if not doctor_speaker_id:
+            if "doctor" in subtitle.content.lower():
+                doctor_name = re.search(r"doctor ([a-z]+)", subtitle.content.lower()).group(1)
+            elif "dr." in subtitle.content.lower():
+                doctor_name = "Dr. " + re.search(r"dr. ([a-z]+)", subtitle.content.lower()).group(1).capitalize()
+            print(f"Doctor name: {doctor_name}")
+            if doctor_name:
+                doctor_speaker_id = re.search(r"\[([A-Z_0-9]+)\]", subtitle.content).group(1)
+                print(f"Doctor speaker ID: {doctor_speaker_id}")
+        elif not patient_speaker_id and doctor_speaker_id not in subtitle.content:
+            patient_speaker_id = re.search(r"\[([A-Z_0-9]+)\]", subtitle.content).group(1)
+            print(f"Patient speaker ID: {patient_speaker_id}")
+
+        if doctor_speaker_id and doctor_speaker_id in subtitle.content:
+            subtitle.content = subtitle.content.replace(doctor_speaker_id, doctor_name)
+        if patient_speaker_id and patient_speaker_id in subtitle.content:
+            subtitle.content = subtitle.content.replace(patient_speaker_id, "Patient")
+
+
         # Create a TextClip for each subtitle
         background_clip = ImageClip(black_background, duration=(subtitle.end - subtitle.start).total_seconds())
         txt_clip = TextClip(subtitle.content, fontsize=35, color='white', font="Arial", size=background_clip.size, method="caption", align="center", interline=0.5)
